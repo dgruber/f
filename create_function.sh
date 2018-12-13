30ecaae0f7d085c33aa98a26cd6eff8dccf28511
Daniel Gruber <mail@danielgruber.com>
2018-12-13 14:05:18 +0100

    f

--- create_function.sh
+++ create_function.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-pfs function create upper --git-repo https://github.com/dgruber/f --artifact lxamd64/f --image $REGISTRY/$REGISTRY_USER/upper --env "FUNCTION=upper" --verbose
+if [ "$1x" = "x" ]; then
+   echo "args: upper / reverse"
+   exit 1
+fi 
+pfs function create $1 --git-repo https://github.com/dgruber/f --artifact lxamd64/f --image $REGISTRY/$REGISTRY_USER/$1 --env "FUNCTION=$1" --verbose
 
